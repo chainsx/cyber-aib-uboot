582ab57ead4b20f9be235659b1131742c2c0eb88
Alvin Xie <alvin@radxa.com>
2024-05-16 15:39:04 +0800

    board: evb_rk3528: add mac_read_from_eeprom support
    Use the mac_read_from_eeprom function as the entry point,
    select different devicetree by reading HW_ID.
    
    Signed-off-by: Alvin Xie <alvin@radxa.com>

--- board/rockchip/evb_rk3528/evb_rk3528.c
+++ board/rockchip/evb_rk3528/evb_rk3528.c
@@ -10,6 +10,23 @@
 #include <linux/usb/phy-rockchip-naneng-combphy.h>
 #include <asm/io.h>
 #include <rockusb.h>
+#include <adc.h>
+#include <dm.h>
+
+#define SARADC_ADDR	"saradc@ffae0000"
+#define HW_ID_CHANNEL	2
+#define BOM_ID_CHANNEL	3
+
+#define countof(x) (sizeof(x) / sizeof(x[0]))
+
+struct variant_def {
+	char *compatible;
+	unsigned int hw_id_lower_bound;
+	unsigned int hw_id_upper_bound;
+	unsigned int bom_id_lower_bound;
+	unsigned int bom_id_upper_bound;
+	char *fdtfile;
+};
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -82,4 +99,66 @@ int board_usb_cleanup(int index, enum usb_init_type init)
 }
 #endif
 
+#endif
+
+#ifdef CONFIG_ID_EEPROM
+static struct variant_def variants[] = {
+	{"radxa,rock-2a",   80, 250, 0, -1, "rockchip/rk3528-rock-2a.dtb"},
+	{"radxa,rock-2a",   320, 380, 0, -1, "rockchip/rk3528-radxa-e20c.dtb"},
+	{"radxa,rock-2a",   530, 710, 0, -1, "rockchip/rk3528-rock-2f.dtb"},
+};
+
+static void set_fdtfile(void)
+{
+	int i, ret;
+	unsigned int hw_id, bom_id;
+	struct variant_def *v;
+
+	ret = adc_channel_single_shot(SARADC_ADDR, HW_ID_CHANNEL, &hw_id);
+	if (ret) {
+		pr_err("%s: adc_channel_single_shot fail for HW_ID: %i!\n", __func__, ret);
+		return;
+	}
+	ret = adc_channel_single_shot(SARADC_ADDR, BOM_ID_CHANNEL, &bom_id);
+	if (ret) {
+		pr_err("%s: adc_channel_single_shot fail for BOM_ID: %i!\n", __func__, ret);
+		return;
+	}
+	for(i = 0; i < countof(variants); i++) {
+		v = &variants[i];
+		if (of_machine_is_compatible(v->compatible) &&
+		    hw_id >= v->hw_id_lower_bound &&
+		    hw_id <= v->hw_id_upper_bound &&
+		    bom_id >= v->bom_id_lower_bound &&
+		    bom_id <= v->bom_id_upper_bound) {
+			printf("Override default fdtfile to %s\n", v->fdtfile);
+			env_set("fdtfile", v->fdtfile);
+			break;
+		}
+	}
+}
+
+/**
+ * mac_read_from_eeprom() - read the MAC address & the serial number in EEPROM
+ *
+ * This function reads the MAC address and the serial number from EEPROM and
+ * sets the appropriate environment variables for each one read.
+ *
+ * The environment variables are only set if they haven't been set already.
+ * This ensures that any user-saved variables are never overwritten.
+ *
+ * If CONFIG_ID_EEPROM is enabled, this function will be called in
+ * "static init_fnc_t init_sequence_r[]" of u-boot/common/board_r.c.
+ */
+int mac_read_from_eeprom(void)
+{
+	set_fdtfile();
+	return 0;
+}
+
+int do_mac(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	printf("This device does not support user programmable EEPROM.\n");
+	return -1;
+}
 #endif
