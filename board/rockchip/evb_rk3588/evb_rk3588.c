a8a093ff31d7c8d82c170980705288efbfff02d2
Mitchell Ma <machuang@radxa.com>
2024-08-30 14:45:30 +0800

    board: evb_rk3588: add startup by reading HW_ID
    Signed-off-by: Mitchell Ma <machuang@radxa.com>

--- board/rockchip/evb_rk3588/evb_rk3588.c
+++ board/rockchip/evb_rk3588/evb_rk3588.c
@@ -10,6 +10,20 @@
 #include <linux/usb/phy-rockchip-usbdp.h>
 #include <asm/io.h>
 #include <rockusb.h>
+#include <adc.h>
+#include <dm.h>
+
+#define SARADC_ADDR	"saradc@fec10000"
+#define HW_ID_CHANNEL	5
+
+#define countof(x) (sizeof(x) / sizeof(x[0]))
+
+struct variant_def {
+	char *compatible;
+	unsigned int hw_id_lower_bound;
+	unsigned int hw_id_upper_bound;
+	char *fdtfile;
+};
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -81,5 +95,64 @@ int board_usb_cleanup(int index, enum usb_init_type init)
 	return 0;
 }
 #endif
+
+#endif
+
+#ifdef CONFIG_ID_EEPROM
+static struct variant_def variants[] = {
+	{"rockchip,rk3588", 300, 380, "rockchip/rk3588s-radxa-e54c.dtb"},
+	{"rockchip,rk3588", 980, 1060, "rockchip/rk3588-rock-5t.dtb"},
+	{"rockchip,rk3588", 1650, 1730, "rockchip/rk3588s-radxa-e52c.dtb"},
+	{"rockchip,rk3588", 2360, 2440, "rockchip/rk3588s-rock-5c.dtb"},
+	{"rockchip,rk3588", 3370, 3450, "rockchip/rk3588s-rock-5d.dtb"},
+	{"rockchip,rk3588", 4050, 4130, "rockchip/rk3588-rock-5b-plus.dtb"},
+};
+
+static void set_fdtfile(void)
+{
+	int i, ret;
+	unsigned int hw_id;
+	struct variant_def *v;
+
+	ret = adc_channel_single_shot(SARADC_ADDR, HW_ID_CHANNEL, &hw_id);
+	if (ret) {
+		pr_err("%s: adc_channel_single_shot fail for HW_ID: %i!\n", __func__, ret);
+		return;
+	}
+	for(i = 0; i < countof(variants); i++) {
+		v = &variants[i];
+		if (of_machine_is_compatible(v->compatible) &&
+		    hw_id >= v->hw_id_lower_bound &&
+		    hw_id <= v->hw_id_upper_bound) {
+			printf("Override default fdtfile to %s\n", v->fdtfile);
+			env_set("fdtfile", v->fdtfile);
+			break;
+		}
+	}
+}
+
+/**
+ * mac_read_from_eeprom() - read the MAC address & the serial number in EEPROM
+ *
+ * This function reads the MAC address and the serial number from EEPROM and
+ * sets the appropriate environment variables for each one read.
+ *
+ * The environment variables are only set if they haven't been set already.
+ * This ensures that any user-saved variables are never overwritten.
+ *
+ * If CONFIG_ID_EEPROM is enabled, this function will be called in
+ * "static init_fnc_t init_sequence_r[]" of u-boot/common/board_r.c.
+ */
+int mac_read_from_eeprom(void)
+{
+	set_fdtfile();
+	return 0;
+}
+
+int do_mac(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	printf("This device does not support user programmable EEPROM.\n");
+	return -1;
+}
 
 #endif
